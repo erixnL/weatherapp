cd01abc4a40cb3bade06b10aebb2d7cf72aa60d2
erixnL <96608889+erixnL@users.noreply.github.com>
2022-12-01 12:41:51 +1100

    fix the image issue, align the icon and text

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', e=>{
         const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city.value}&aqi=no`;
         fetch(url).then(response=> response.json()).then(data => {
             var dataSet = {
-                "icon": data.current.condition.icon,
+                "icon": "https://" + data.current.condition.icon.split('//')[1],
                 "text": data.current.condition.text,
                 "temp_c": data.current.temp_c,
                 "name": data.location.name,
