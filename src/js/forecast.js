d553ef08d082844f1adbebf472263215a0a6509f
erixnL <96608889+erixnL@users.noreply.github.com>
2022-12-13 15:44:52 +1100

    hiden default weather in small screen

--- src/js/forecast.js
+++ src/js/forecast.js
@@ -14,11 +14,11 @@ const weatherForecast = function() {document.getElementById('forecastBtn').addEv
     if (validation()) {
         const url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city.value}&days=5&aqi=no&alerts=no`;
         fetch(url).then(response=> response.json()).then(data => {
-            listContent +=  `<li><span class="resultHeadLine">${data.location.name}, ${data.location.country} in Next Five Days:<span></li>`
+            listContent +=  `<li><span class="resultHeadLine">${data.location.name}, ${data.location.country} in Next Three Days:<span></li>`
             var forecastData = data.forecast.forecastday;
             forecastData.forEach(element=>{
                     var perDay = {
-                        "date": element.date,
+                        "date": element.date.slice(5),
                         "max": element.day.maxtemp_c,
                         "min":element.day.mintemp_c,
                         "icon": "https:" + element.day.condition.icon,
@@ -36,9 +36,13 @@ const weatherForecast = function() {document.getElementById('forecastBtn').addEv
         }).catch(() => {
             invalidCity.style.display = 'block';
         });
+        if(window.innerWidth <= 700){
+            document.getElementById('defaultCity').style.display = 'none';
+        }
+        
     }
     }
-)
+    );
 }
 
 export {weatherForecast};
