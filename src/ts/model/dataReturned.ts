2de04277a82ae2608a7c4db325c5dd5200aa5387
erixnL <96608889+erixnL@users.noreply.github.com>
2023-01-04 19:41:16 +1100

    revert to then/catch error method

--- src/ts/model/dataReturned.ts
+++ src/ts/model/dataReturned.ts
@@ -1,10 +1,6 @@
 import { showInvalidError } from "../lib/errorMsg";
 export async function returnApiInfo(targetUrl:string) {
-    try {
         const response: Response = await fetch(targetUrl);
         const returnedInfo = await response.json();
         return returnedInfo;
-    } catch(error) {
-        showInvalidError();
-    }
 }
