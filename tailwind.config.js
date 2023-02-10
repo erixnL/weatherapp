9ab6b3ab5b95795ceea8f74971511c29f04d8f4a
erixnL <96608889+erixnL@users.noreply.github.com>
2023-02-10 19:42:31 +1100

    add tailwind css class for html element;add cityname class in output.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
+const { reduceEachLeadingCommentRange } = require('typescript');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/index.html'],
   theme: {
     extend: {
       colors: {
-        summerBlue: '#2364aa',
-        skyBlue: '#3da5d9',
-        greenSheen: '#73bfb8',
-        ginerYellow: '#fec601',
-        nectarineOrange:'#ea7317'
+        summerBlue: '#004e98',
+        skyBlue: '#3a6ea5',
+        deepGrey: '#c0c0c0',
+        lightGrey: '#ebebeb',
+        brightOrange:'#ff6700',
+        errorRed: '#d62828',
       }
     },
   },
